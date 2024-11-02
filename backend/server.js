4bf2c8205c2d33ac8ae6263b01ba86f5883b82bc
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-11-01 21:22:53 -0500

    Se cambia puerto de frontend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" } // Cambia el puerto según el que uses para el cliente Vite
+  cors: { origin: "http://localhost:8000" } // Cambia el puerto según el que uses para el cliente Vite
 });
 
 io.on('connection', (socket) => {
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000, // Cambia 3000 al puerto que prefieras
+  },
 })
